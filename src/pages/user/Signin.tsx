d385335921c4ce0a7386b106b98eb014974079d7
astar-dew <qwedcxzas8@naver.com>
2026-01-28 14:56:13 +0900

    design : 로그인 , 추가 컴포넌트 및 아이콘 추가
    추가된 component 변경 및 css 변경

--- src/pages/user/Signin.tsx
+++ src/pages/user/Signin.tsx
@@ -1,16 +1,18 @@
 
-import DefaultButton from '../../component/button/DefaultButton';
 import { useNavigate } from "react-router-dom";
 import MockData from '../../data/User'
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { z } from "zod";
 import { zodResolver } from '@hookform/resolvers/zod';
+import STBasicLabelInput from '../../component/common/input/STBasicLabelInput';
+
+import { Cable, Eye, EyeOff, Flower2, LogIn, UserPlus } from 'lucide-react'
+import STBasicButton from '../../component/common/button/STBasicButton';
 
 type SignInForm = {
     loginId: string,
     loginPass: string
-    
 }
 
 const formSchem = z.object({
@@ -23,9 +25,12 @@ const formSchem = z.object({
 export default function Signin(){
     const nav = useNavigate();
 
-    const {mockUserList} = MockData()
+    const { mockUserList } = MockData()
 
-    const [userList , setUserList] = useState(mockUserList);
+    const [ userList , setUserList ] = useState(mockUserList);
+
+    const [ isPassVisible, setIsPassVisible] = useState(false);
+    const [ passwordType , setPasswordType] = useState('password')
 
     // const [ formData , setFormData ] = useState<SignInForm>({
     //         loginId:'',
@@ -46,14 +51,22 @@ export default function Signin(){
     //     // => 제네릭을 넣지 않으면 
     //     // HTMLSelectElement: <select> 태그에서 발생한 이벤트임을 명시
     //     // TypeScript는 e.target이 정확히 무엇인지 알 수 없어 value나 name 같은 속성에 접근할 때 에러를 발생시킵니다.
-    //     console.log('handle event : ',e)
     //     const { name, value } = e.target;
     //     setFormData(prev =>({...prev, [name]: value}))
     // }
 
+
+    const visibleUpdate = () => {
+        setIsPassVisible(!isPassVisible)
+        isPassVisible? setPasswordType('password') :  setPasswordType('text')
+    }
+
     const signIn = () => {
         //mock data에서 정보 확인. 
         const signInResult = mockUserList.filter((user)=> user.userId === watch('loginId') && user.pass === watch('loginPass'));
+        
+        // console.log('watch login : ',watch('loginId'))
+        // console.log('watch loginPass : ',watch('loginPass'))
 
         //local storage 에 저장 (새로 고침에도 데이터 유지), usestate에 저장 (새로고침 전 데이터 유지 )
         if(signInResult.length == 0){
@@ -71,26 +84,59 @@ export default function Signin(){
         nav('/sign-up')
     }
 
-    
-    
     return (
         <form id="signInForm" onSubmit={handleSubmit((e)=>{})} >
-            <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100  p-4">
-                <div className='flex flex-col bg-white rounded-2xl shadow-xl p-4'>
-                        <label htmlFor="loginId">id</label>
-                        <input id="loginId" className='border p-2' type="text"  {...register('loginId')}/>
-                        {errors.loginId&&<p>{errors.loginId.message}</p>}
+            <div className="flex justify-center bg-black ">
+                <div className='flex flex-col items-center bg-white rounded-2xl shadow-xl p-4 space-y-3'>
 
-                        <label htmlFor="loginPass">password</label>
-                        <input id="loginPass" className='border p-2' type="password"  {...register('loginPass')} />
-                        {errors.loginPass&&<p>{errors.loginPass.message}</p>}
+                    <section className='flex flex-col items-center p-10'>
+                        <h1 className=''><Flower2 color={'red'}className='inline'/>Welcom Back!<Flower2 color={'red'}className='inline'/></h1>
+                        <h3 className=''>We missed your Please enter your details</h3>
+                    </section>
 
-                        <div className='flex justify-center'>
-                            <DefaultButton buttonText='sign in' handleClick={signIn}/> 
-                            <DefaultButton buttonText='sign up' handleClick={signUp}/> 
-                        </div>
+                    <STBasicLabelInput
+                        id='loginId'
+                        labelText='아이디'
+                        placeholder='Enter your Id'
+                        labelClassName='block p-2 '
+                        inputClassName='border p-2 rounded-2xl'
+                        {...register('loginId')}
+                    />
+                    {errors.loginId && <p>{errors.loginId.message}</p>} 
+
+                    <STBasicLabelInput
+                        id='loginPass'
+                        type={passwordType}
+                        labelText='패스워드'
+                        placeholder='Enter Password'
+                        labelClassName='block p-2 '
+                        inputClassName='border p-2 rounded-2xl'
+                        icon={isPassVisible? <EyeOff className="inline" onClick={visibleUpdate}/>
+                                            :<Eye className="inline" onClick={visibleUpdate}/> }
+                        {...register('loginPass')}
+                    />
+                    {errors.loginPass && <p>{errors.loginPass.message}</p>} 
+                    
+                    <STBasicButton
+                        className='w-full border m-1 p-4 rounded-2xl' 
+                        onClick={signIn}
+                    >
+                        <LogIn className='inline mr-2'/>Sign In 
+                        {/* inline 빼는 방향으로 */}
+                    </STBasicButton>
+
+                    <STBasicButton
+                        className='w-full border m-1 p-4 rounded-2xl ' 
+                        onClick={signUp}
+                    >
+                        <UserPlus className='inline mr-2'/>Sign Up
+                    </STBasicButton>
+                    
                 </div>
             </div>
         </form>
     )
 }
+
+// register의 역할이 궁금함. 
+// zod의 실행 로직에 대해서 궁금함. 
