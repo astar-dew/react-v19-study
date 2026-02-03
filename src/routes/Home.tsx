c6d05c9febcdd9a7106d89308a90dbeab8b16b2c
astar-dew <qwedcxzas8@naver.com>
2026-02-03 10:34:43 +0900

    design : Home 화면  비율이 반으로 짤리는 부분 , 임시 조치
    home layout 에 min-h-screen w-full 을 넣어 화면 전체로
    header 영역때문에 기본 영역보다 더 길어질수있는점을 체크 필요.
    원인은 tailwind version 때문에 생겼을 것으로 보임.

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -17,14 +17,15 @@ export default function Home(){
     // }
 
     return (
-        <div>
-        HOME
+        <div className=' min-h-screen w-full'>
+            HOME
+
         </div>
     )
 }
 
 
-
+// 일단 여기서 테스트했을때 home 글자만 있을때 그 박스라인에 컴팩트하게 잡힘.
 
 // const Home: FC<HomeProps> = ({title}) => {
 //     return (<>
